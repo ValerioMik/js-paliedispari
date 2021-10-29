c571230f4de10eed7a91ced658bffe7235b78aba
ValerioMik <valerio.avallone@hotmail.com>
2021-10-29 15:19:08 +0200

    commento primo esercizio termitato, ora sviluppo

---
+++ dispari/script.js
@@ -0,0 +1,13 @@
+/* Palidroma:
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma.
+Dare l’output relativo. */
+
+//1.inserisco un prompt per chiedere la parola all'utente
+    //1.1.faccio si che la parola venga inserita in una variabile
+    //1.2faccio si che la parola inserita venga girata
+        //inserisco questultima in un altra variabile
+//2. comparo le due parole
+    //2.1se la parola inserita dall'utente e uguale alla parola girata
+    //2.2 stampo "si la parola inserita e palindroma
+    //2.3 altrimenti stampo no la parola non è palindroma
