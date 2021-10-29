08c8dedf26d29700a0d5dbc63373e17c8ee35124
ValerioMik <valerio.avallone@hotmail.com>
2021-10-29 16:06:15 +0200

    esercizio 1 terminato

--- dispari/script.js
+++ dispari/script.js
@@ -3,11 +3,35 @@ Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma.
 Dare l’output relativo. */
 
-//1.inserisco un prompt per chiedere la parola all'utente
-    //1.1.faccio si che la parola venga inserita in una variabile
-    //1.2faccio si che la parola inserita venga girata
-        //inserisco questultima in un altra variabile
-//2. comparo le due parole
-    //2.1se la parola inserita dall'utente e uguale alla parola girata
-    //2.2 stampo "si la parola inserita e palindroma
-    //2.3 altrimenti stampo no la parola non è palindroma
+//DONE=>1.inserisco un prompt per chiedere la parola all'utente
+//DONE=>1.1.faccio si che la parola venga inserita in una variabile
+//DONE=>1.2faccio si che la parola inserita venga girata tramite una funzione
+//DONE=>inserisco questultima in un altra variabile
+//2.DONE=> comparo le due parole tramite una funzione 
+//2.1DONE=>se la parola inserita dall'utente e uguale alla parola girata
+//2.2DONE=> stampo "si la parola inserita e palindroma"
+//2.3DONE=> altrimenti stampo no la parola non è palindroma
+
+var parolaInserita = prompt("inserisci una parola");
+var parolainversa = invertiParola(parolaInserita);
+
+if (comparaParola(parolaInserita, parolainversa)) {
+    console.log("si la parola inserita e palindroma");
+}else {
+    console.log("no la parola inserita e palindroma");
+}
+
+
+
+
+    function invertiParola(stringa) {
+        var stringaInversa =
+            stringa.split('').reverse().join('');
+        return stringaInversa;
+    }
+
+    function comparaParola(stringa1, stringa2) {
+        var controllo =
+            stringa1 == stringa2;
+        return controllo;
+    }
